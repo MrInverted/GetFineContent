98ea0bae50100fe43672b33ef6b5a5651061fe16
MrInverted <akson.dyuk@gmail.com>
2024-09-10 13:22:17 +0300

    13:22

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
 try {
   const burgerTrigger = document.querySelector("#burger-trigger");
   const burgerContent = document.querySelector("#burger-content");
+  const links = document.querySelectorAll("#burger-content a");
+
+  const closeBurger = () => {
+    burgerTrigger.classList.remove("active");
+    burgerContent.classList.remove("active");
+  }
 
   burgerTrigger.onclick = () => {
     burgerTrigger.classList.toggle("active");
     burgerContent.classList.toggle("active");
   }
+
+  links.forEach(link => {
+    link.onclick = closeBurger;
+  });
+
 } catch (error) {
   console.error("burger catch,", error)
 }
